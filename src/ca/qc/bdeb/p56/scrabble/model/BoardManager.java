630acd57c301286cc737849a6b8b87a8764c3d8a
TheFrenchOne <pascal@pascallaprade.com>
2016-11-06 18:47:42 -0500

    Test

--- src/ca/qc/bdeb/p56/scrabble/model/BoardManager.java
+++ src/ca/qc/bdeb/p56/scrabble/model/BoardManager.java
@@ -196,6 +196,6 @@ public class BoardManager {
             }
             candidats.remove(candidatAnalysing);
         }
-        return squaresAvailable;
+        return squaresAvailable; // test
     }
 }
